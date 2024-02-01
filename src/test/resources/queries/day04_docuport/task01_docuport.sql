168a9913e71d5622e7531a93945afacb299e5833
Ahmad <ahmad.mammadli97@gmail.com>
2024-01-31 23:31:42 -0500

    #jdbc

--- src/test/resources/queries/day04_docuport/task01_docuport.sql
+++ src/test/resources/queries/day04_docuport/task01_docuport.sql
@@ -1,3 +1,4 @@
 select * from document.users;
 select * from document.users where first_name = 'Alex';
 select * from document.users where department_id is not null;
+select * from document.users where last_name = 'krt';
